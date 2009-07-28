7a3c78464f76a81065bb7d0888fa075554185e67
Charles Cook <chascook@gmail.com>
2009-07-28 11:58:26 +0000

    Issue 55: change XmlRpcProxyGen so that FileIOPermission is not required for calls to Create.
    git-svn-id: http://xmlrpcnet.googlecode.com/svn/trunk@86 c2c9eef6-743f-0410-bebf-e374e69913be

--- src/XmlRpcProxyGen.cs
+++ src/XmlRpcProxyGen.cs
@@ -42,6 +42,7 @@ namespace CookComputing.XmlRpc
 
     public static object Create(Type itf)
     {
+      // create transient assembly
       Type proxyType;
       lock (typeof(XmlRpcProxyGen))
       {
@@ -52,7 +53,7 @@ namespace CookComputing.XmlRpc
           string moduleName = "XmlRpcProxy" + guid.ToString() + ".dll";
           string typeName = "XmlRpcProxy" + guid.ToString();
           AssemblyBuilder assBldr = BuildAssembly(itf, assemblyName,
-            moduleName, typeName);
+            moduleName, typeName, AssemblyBuilderAccess.Run);
           proxyType = assBldr.GetType(typeName);
           _types.Add(itf, proxyType);
         }
@@ -67,11 +68,12 @@ namespace CookComputing.XmlRpc
       string assemblyName
       )
     {
+      // create persistable assembly
       if (assemblyName.IndexOf(".dll") == (assemblyName.Length - 4))
         assemblyName = assemblyName.Substring(0, assemblyName.Length - 4);
       string moduleName = assemblyName + ".dll";
       AssemblyBuilder assBldr = BuildAssembly(itf, assemblyName,
-        moduleName, typeName);
+        moduleName, typeName, AssemblyBuilderAccess.RunAndSave);
       Type proxyType = assBldr.GetType(typeName);
       object ret = Activator.CreateInstance(proxyType);
       assBldr.Save(moduleName);
@@ -82,7 +84,8 @@ namespace CookComputing.XmlRpc
       Type itf,
       string assemblyName,
       string moduleName,
-      string typeName)
+      string typeName,
+      AssemblyBuilderAccess access)
     {
       string urlString = GetXmlRpcUrl(itf);
       List<MethodData> methods = GetXmlRpcMethods(itf);
@@ -90,11 +93,13 @@ namespace CookComputing.XmlRpc
       List<MethodData> endMethods = GetXmlRpcEndMethods(itf);
       AssemblyName assName = new AssemblyName();
       assName.Name = assemblyName;
-      assName.Version = itf.Assembly.GetName().Version;
+      if (access == AssemblyBuilderAccess.RunAndSave)
+        assName.Version = itf.Assembly.GetName().Version;
       AssemblyBuilder assBldr = AppDomain.CurrentDomain.DefineDynamicAssembly(
-        assName, AssemblyBuilderAccess.RunAndSave);
-      ModuleBuilder modBldr = assBldr.DefineDynamicModule(assName.Name,
-        moduleName);
+        assName, access);
+      ModuleBuilder modBldr = (access == AssemblyBuilderAccess.Run
+        ?assBldr.DefineDynamicModule(assName.Name)
+        :assBldr.DefineDynamicModule(assName.Name, moduleName));
       TypeBuilder typeBldr = modBldr.DefineType(
         typeName,
         TypeAttributes.Class | TypeAttributes.Sealed | TypeAttributes.Public,
