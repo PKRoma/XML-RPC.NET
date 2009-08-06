0931b56e93ea61844bac36a8ca884a74b4fab3a1
Charles Cook <chascook@gmail.com>
2009-08-06 08:19:51 +0000

    Issue 56: Maintain XML-RPC struct member ordering in XmlRpcStruct after deserialization.
    git-svn-id: http://xmlrpcnet.googlecode.com/svn/branches/2-4@106 c2c9eef6-743f-0410-bebf-e374e69913be

--- ntest/parsetest.cs
+++ ntest/parsetest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.IO;
 using System.Xml;
 using System.Reflection;
@@ -972,6 +973,71 @@ namespace ntest
 </value>";
       object obj = Utils.Parse(xml, typeof(Struct5), MappingAction.Error,
         out parsedType, out parsedArrayType);
+    }
+
+
+    struct Struct6
+    {
+      [NonSerialized]
+      public decimal x;
+      public int y;
+    }
+
+    [Test]
+    public void NonSerializedNonXmlRpcType()
+    {
+      Type parsedType, parsedArrayType;
+      string xml = @"<?xml version=""1.0"" ?>
+<value>
+  <struct>
+    <member>
+      <name>y</name>
+      <value><i4>18</i4></value>
+    </member>
+  </struct>
+</value>";
+      object obj = Utils.Parse(xml, typeof(Struct4), MappingAction.Error,
+        out parsedType, out parsedArrayType);
+      Struct4 ret = (Struct4)obj;
+      Assert.AreEqual(0, ret.x);
+      Assert.AreEqual(18, ret.y);
+    }
+
+    [Test]
+    public void XmlRpcStructOrder()
+    {
+      Type parsedType, parsedArrayType;
+      string xml = @"<?xml version=""1.0"" ?>
+<value>
+  <struct>
+    <member>
+      <name>a</name>
+      <value><i4>1</i4></value>
+    </member>
+    <member>
+      <name>c</name>
+      <value><i4>3</i4></value>
+    </member>
+    <member>
+      <name>b</name>
+      <value><i4>2</i4></value>
+    </member>
+  </struct>
+</value>";
+      object obj = Utils.Parse(xml, typeof(XmlRpcStruct), MappingAction.Error,
+        out parsedType, out parsedArrayType);
+      Assert.IsInstanceOfType(typeof(XmlRpcStruct), obj);
+      XmlRpcStruct strct = obj as XmlRpcStruct;
+      IDictionaryEnumerator denumerator = strct.GetEnumerator();
+      denumerator.MoveNext();
+      Assert.AreEqual("a", denumerator.Key);
+      Assert.AreEqual(1, denumerator.Value);
+      denumerator.MoveNext();
+      Assert.AreEqual("c", denumerator.Key);
+      Assert.AreEqual(3, denumerator.Value);
+      denumerator.MoveNext();
+      Assert.AreEqual("b", denumerator.Key);
+      Assert.AreEqual(2, denumerator.Value);
     }
   }
 }
