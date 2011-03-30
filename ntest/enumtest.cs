dd95f6c37aaa709a259497fcba6ce7cdcc13e4e2
Charles Cook <chascook@gmail.com>
2011-03-30 05:52:02 +0000

    Add enum tests
    git-svn-id: http://xmlrpcnet.googlecode.com/svn/trunk@223 c2c9eef6-743f-0410-bebf-e374e69913be

---
+++ ntest/enumtest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using CookComputing.XmlRpc;
+using NUnit.Framework;
+
+namespace ntest
+{
+  public enum TestEnum
+  {
+    a,
+    b,
+    c,
+  }
+
+  class enumtest
+  {
+    [Test]
+    public void Serialize()
+    {
+      Stream stm = new MemoryStream();
+      XmlRpcRequest req = new XmlRpcRequest();
+      req.args = new Object[] { TestEnum.c };
+      req.method = "Foo";
+      var ser = new XmlRpcRequestSerializer();
+      ser.SerializeRequest(stm, req);
+      stm.Position = 0;
+      TextReader tr = new StreamReader(stm);
+      string reqstr = tr.ReadToEnd();
+
+      Assert.AreEqual(
+        @"<?xml version=""1.0""?>
+<methodCall>
+  <methodName>Foo</methodName>
+  <params>
+    <param>
+      <value>
+        <i4>2</i4>
+      </value>
+    </param>
+  </params>
+</methodCall>", reqstr);
+    }
+
+    [Test]
+    public void Deserialize()
+    {
+      string xml = 
+@"<?xml version=""1.0"" encoding=""iso-8859-1""?>
+<methodResponse>
+  <params>
+    <param>
+      <value>
+        <i4>2</i4>
+      </value>
+    </param>
+  </params>
+</methodResponse>";
+      StringReader sr1 = new StringReader(xml);
+      var deserializer = new XmlRpcResponseDeserializer();
+      XmlRpcResponse response = deserializer.DeserializeResponse(sr1,
+        typeof(TestEnum));
+      TestEnum ret = (TestEnum)response.retVal;
+      Assert.AreEqual(TestEnum.c, ret);
+    }
+  }
+}
