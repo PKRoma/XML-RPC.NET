89f2d18259df55b61ca3dc2cacb68ee5322188d4
Charles Cook <chascook@gmail.com>
2011-07-21 08:08:33 +0000

    Changes to Silverlight unit tests
    git-svn-id: http://xmlrpcnet.googlecode.com/svn/trunk@263 c2c9eef6-743f-0410-bebf-e374e69913be

--- ntest/NilTest.cs
+++ ntest/NilTest.cs
@@ -241,76 +241,6 @@ namespace ntest
     }
 
     [Test]
-    public void DeserializeRequestStructWithNil()
-    {
-      string xml = @"<?xml version=""1.0""?>
-<methodCall>
-    <methodName>StructWithArrayMethod</methodName>
-    <params>
-        <param>
-            <value>
-                <struct>
-                    <member>
-                        <name>ints</name>
-                        <value>
-                            <array>
-                                <data>
-                                    <value>
-                                        <i4>1</i4>
-                                    </value>
-                                    <value>
-                                        <nil />
-                                    </value>
-                                    <value>
-                                        <i4>3</i4>
-                                    </value>
-                                </data>
-                            </array>
-                        </value>
-                    </member>
-                </struct>
-            </value>
-        </param>
-    </params>
-</methodCall>";
-      StringReader sr = new StringReader(xml);
-      var deserializer = new XmlRpcRequestDeserializer();
-      XmlRpcRequest request = deserializer.DeserializeRequest(sr, this.GetType());
-
-      Assert.AreEqual(request.method, "StructWithArrayMethod", "method is TestString");
-      Assert.AreEqual(1, request.args.Length);
-      Assert.IsInstanceOf<StructWithArray>(request.args[0], "argument is StructWithArray");
-      int?[] arg = ((StructWithArray)request.args[0]).ints;
-      Assert.AreEqual(1, arg[0]);
-      Assert.IsNull(arg[1]);
-      Assert.AreEqual(3, arg[2]);
-    }
-
-    [Test]
-    public void DeserializeRequestNilMethod()
-    {
-      string xml = @"<?xml version=""1.0"" ?> 
-<methodCall>
-  <methodName>NilMethod</methodName> 
-  <params>
-    <param>
-      <value><nil /></value>
-    </param>
-    <param>
-      <value><int>12345</int></value>
-    </param>
-  </params>
-</methodCall>";
-      StringReader sr = new StringReader(xml);
-      var deserializer = new XmlRpcRequestDeserializer();
-      XmlRpcRequest request = deserializer.DeserializeRequest(sr, this.GetType());
-
-      Assert.AreEqual(request.method, "NilMethod", "method is TestString");
-      Assert.IsNull(request.args[0], "argument is null");
-      Assert.AreEqual(12345, (int)request.args[1], "argument is 12345");
-    }
-
-    [Test]
     public void DeserializeNilObject()
     {
       string xml = "<value><nil /></value>";
@@ -320,7 +250,7 @@ namespace ntest
   }
 
   [XmlRpcNullMapping(NullMappingAction.Nil)]
-  class Bounds
+  public class Bounds
   {
     public int? lowerBound;
     public int? upperBound;
